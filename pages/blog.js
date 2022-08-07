9778dbc14b6d4dcd6e4fa91eb5ebf87c43c0af5d
Nicolas Sierra <nico.sierra13@gmail.com>
2022-08-07 14:16:50 -0500

    blog routing created

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Blog = () => {
+  return (
+    <Layout title="Blog">
+      <h1 className="heading"> Desde blog </h1>
+    </Layout>
+  );
+};
+
+export default Blog;
